6500a50bf9ea23eac5f4e7d8df83a55015d0e557
Siddharth Kumar <sidkumar@live.com>
2020-04-20 13:05:52 -0500

    created class

--- scrapeIG.py
+++ scrapeIG.py
@@ -8,42 +8,56 @@ import json, re
 import pprint as pp
 import time
 
-USERNAME = 'sidkumar17'
-IMAGES = []
-URL = 'https://www.instagram.com/' + USERNAME 
-WAIT_TIME = 20
-MAX_PAGES = 1
+class Scraper:
+    def __init__(self, user_name, wait_time=20, max_pages=1):
+       
+        self.USERNAME = user_name
+        
+        self.URL = 'https://www.instagram.com/' + USERNAME 
+        self.WAIT_TIME = wait_time
+        self.MAX_PAGES = max_pages
 
-s = requests.Session()
-s.headers['user-agent'] = 'Mozilla/5.0'
+        
+    def scrape(self):
+        s = requests.Session()
+        s.headers['user-agent'] = 'Mozilla/5.0'
+        end_cursor = ''
+        images = []
 
-end_cursor = ''
+        for x in range(MAX_PAGES):
 
+            r    = s.get(URL)
+            data = re.search(r'window._sharedData = (\{.+?});</script>', r.text).group(1)
 
+            j = json.loads(data)
+            j = j['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']
 
-for x in range(MAX_PAGES):
+            end = j['page_info']['has_next_page']
+            media = j['edges'] 
 
-    r    = s.get(URL)
-    data = re.search(r'window._sharedData = (\{.+?});</script>', r.text).group(1)
+            for x in media:
+                images.append(x['node']['display_url'])
+            
+            if not end:
+                break
+            else:
+                time.sleep(WAIT_TIME)
+        s.close()
+        return images
 
-    j = json.loads(data)
-    j = j['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']
-
-    end = j['page_info']['has_next_page']
-    media = j['edges'] 
-
-    for x in media:
-        IMAGES.append(x['node']['display_url'])
-    
-    if not end:
-        break
-    else:
-        time.sleep(WAIT_TIME)
-
-#pp.pprint(IMAGES)
-x=0
-for img in IMAGES:
-    urllib.request.urlretrieve(img, 'img/'+USERNAME+'_IMG_'+str(x)+'.jpg')
-    x+=1
-
-s.close()
+        #pp.pprint(IMAGES)
+    def getImagesFromSource(self,images, path = ''):
+        output_path=[]
+        if path=='':
+            for x in range(len(images)):
+                output_path .append( 'img/'+USERNAME+'_IMG_'+str(x)+'.jpg' )
+        else:
+            for x in range(len(images)):
+                output_path .append( path+str(x)+'.jpg' )
+        
+       
+        for x in range(len(images)):
+            urllib.request.urlretrieve(images[x],output_path[x])
+            
+        return 0
+        
